33ab4325ed0ca2c026e73a4bd36676aceb7ba682
Ben Fields <ben@musicmetric.com>
2012-03-09 18:56:40 +0000

    setup is minimally functional;entities at top

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys, os.path
+import ConfigParser
+from distutils.core import setup
+
+setup(name='mmpy',
+      version='0.0.1',
+      description='Musicmetric API Wrapper',
+      long_description="""This is the officially supported python wrapper for Musicmetric http api.  This API provides direct access to much of the data we use to drive our products.  This data is available for a wide array of music artists; in general, the more popular an artist is, the more likely there is going to be data.
+
+      We provide the following data and services:
+
+      - Time series data of fan activity on a set of social networks,
+      - Sentiment analysis service for music related text
+      - Fan demographics
+      - a variety of lists and charts""",
+      author='Ben Fields',
+      author_email='ben@musicmetric.com',
+      url='https://github.com/musicmetric/mmpy',
+      packages=['mmpy'],
+      package_dir={'mmpy':'src'},
+      provides=['mmpy'],
+      requires=['simplejson']
+      )
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from entity import Entity
+from artist import Artist
+from list import List
